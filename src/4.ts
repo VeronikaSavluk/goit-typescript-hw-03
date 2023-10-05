8367f459fb5ca3e59dff4dbe5076ff64d10d607f
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2023-10-05 18:14:35 +0300

    task 4

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,43 @@
+class Key {
+	private signature = Math.random();
+
+	getSignature() {
+		return this.signature;
+	}
+}
+class Person {
+	constructor(private key: Key){};
+
+	getKey() {
+		return this.key;
+	}
+}
+
+abstract class House{
+protected door = false;
+public key: Key;
+public tenants: Person[] = [];
+
+public abstract openDoor(key: Key): void;
+
+comeIn(tenant: Person){
+	if(this.door === true) {
+	this.tenants.push(tenant);
+	}
+}
+}
+
+class MyHouse extends House {
+	constructor(key){
+		super();
+	}
+openDoor(key){
+if(key === this.key){
+	this.door = true;
+};
+};
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
